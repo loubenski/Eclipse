f729c18f7aa309beef2e9443df7baca83c88b9dc
Vladimir Loubenski <vloubens@gmail.com>
2023-11-22 20:38:44 -0500

    iNITIAL COMMIT

---
+++ src/com/test/eXAMPLES.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.test;
+
+/**
+ * 
+ */
+public class eXAMPLES {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
